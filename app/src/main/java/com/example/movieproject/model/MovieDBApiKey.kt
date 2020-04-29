49c6d3e254d0443a6feae2831251d36f1397d005
Ashkeyev Ali <ashkeyevli@gmail.com>
2020-04-29 14:01:55 +0600

    push_firebase

--- app/src/main/java/com/example/movieproject/model/MovieDBApiKey.kt
+++ app/src/main/java/com/example/movieproject/model/MovieDBApiKey.kt
@@ -1,3 +1,8 @@
 package com.example.movieproject.model
 
 const val MovieDBApiKey = "83d38fb87ade8508f88504700b29d8b3"
+
+const val NAME = "name"
+const val BODY = "body"
+const val CHANNEL = "main"
+const val FILM_URL = "https://www.themoviedb.org/movie"
